a037476fd7baab72005bb25eaae5546745bdda5b
Ivan Kirillov <van-kirillov@yandex.ru>
2023-07-11 12:34:15 +0300

    add xml file

--- gnivcXml/Program.cs
+++ gnivcXml/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using gnivcXml;
+using System.IO;
 
-const string xmlPath = @"C:\Users\van-k\OneDrive\Рабочий стол\ГНИВЦ\UT_ZVREGBENZ_5252_5252_5252000777525201001_20210125_7F167963-6BF9-494E-90BB-461759218EY1.xml";
+//var currentpath = Directory.GetCurrentDirectory();
+const string xmlPath = @"..\..\..\..\UT_ZVREGBENZ_5252_5252_5252000777525201001_20210125_7F167963-6BF9-494E-90BB-461759218EY1.xml";
+//currentpath = Path.GetFullPath(Path.Combine(currentpath, xmlPath));
+
 
 var objFile = new XmlSerialize().ConvertXmlToObject<gnivcXml.zvregbenz.File>(xmlPath);
 objFile.SaveMetaData(xmlPath);
